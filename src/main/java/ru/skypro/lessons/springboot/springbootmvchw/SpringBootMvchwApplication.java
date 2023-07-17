b369907de54bc7ea0cebcb861da02402234be20d
Irina <sheldipower@gmail.com>
2023-07-17 13:21:13 +0500

    первый коммит домашней работы

---
+++ src/main/java/ru/skypro/lessons/springboot/springbootmvchw/SpringBootMvchwApplication.java
@@ -0,0 +1,13 @@
+package ru.skypro.lessons.springboot.springbootmvchw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootMvchwApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootMvchwApplication.class, args);
+    }
+
+}
